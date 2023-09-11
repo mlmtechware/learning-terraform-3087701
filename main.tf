d7b3c21694bc24bbd067f45fa3efcc42031173e4
mlmtechware <141271060+mlmtechware@users.noreply.github.com>
2023-09-11 15:17:01 -0600

    Created dev vpc

--- main.tf
+++ main.tf
@@ -17,16 +17,16 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -45,8 +45,8 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog"
 
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]

e4e5efd9d6a83dba1169383d35d1aab9d1003e9d
mlmtechware <141271060+mlmtechware@users.noreply.github.com>
2023-09-11 15:38:39 -0600

    Created load balancer

--- main.tf
+++ main.tf
@@ -46,17 +46,13 @@ module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "my-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
 
   target_groups = [
     {

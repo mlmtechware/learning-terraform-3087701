38b2f606852c4879db022322faf9e5e6884c83ad
mlmtechware <141271060+mlmtechware@users.noreply.github.com>
2023-09-11 16:21:10 -0600

    Changed target id for autoscaling

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "blog_alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
+          target_id = autoscaling.image_id
           port = 80
         }
       }
